4327ff98f5e2a4491f508fe4975a1a8000a2bea0
nicoreed <dev@nicoreed.com>
2011-06-28 19:46:35 -0700

    [minor] claned up the example client

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 var dnode = require('dnode');
 
 dnode.connect(8080, function (remote) {
-  remote.getGuide('what-are-callbacks', function () {
-    console.log(arguments);
+  remote.getGuide('what-are-callbacks', function (err, guide) {
+    console.log(guide);
   });
 
-  remote.getGuides('core', function () {
-    console.log(arguments);
+  remote.getGuides('core', function (err, list) {
+    console.log(list);
   });
 
-  remote.getTags(function () {
-    console.log(arguments);
+  remote.getTags(function (err, tags) {
+    console.log(tags);
   });
 });

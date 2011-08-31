bedf7cfffcc282a52e8395fc6f375d41ba43035e
hij1nx <fernmicro@gmail.com>
2011-08-31 17:10:24 -0400

    [ui] ui behavior

--- theme/js/index.js
+++ theme/js/index.js
@@ -1,104 +1,33 @@
 $(function() {
+
+  var noop = function() {
+    return false;
+  };
   
-  var selectingTopicTimer,
-  selectingSubtopicTimer,
-  selected;
+  var t;
 
-  //
-  // When the user's mouse enters a subtopic,
-  // assume the intent is to select the subtopic
-  //
-  $('#toc > ul > li > ul > li').mouseenter(function() {
-    clearInterval(selectingTopicTimer);
-    clearInterval(selectingSubtopicTimer);
-  });
-
-
-  //
-  // When the user's mouse enters a topic,
-  // assume the intent is to select a topic
-  //
-  $('#toc > ul > li > a').mouseenter(function() {
-
-    clearTimeout(selectingTopicTimer);
-    clearTimeout(selectingSubtopicTimer);
-
-    var topic = this;
-
-    //
-    // Don't re-open the same topic twice
-    //
-    if($(topic).hasClass('selected')) {
-      // console.log('already selected, dont reopen');
+  $('#toc > ul > li > a').click(function() {
+    clearTimeout(t);
+    if($(this).hasClass('selected')) {
       return;
     }
-
-    //
-    // If there is no selected topic, immediately open one
-    //
-    if(!isTopicSelected()){
-      //console.log('no topic, just open it quickly');
-      openTopic(topic);
-      return false;
-    }
-
-    //
-    // Instead of immediately opening a new topic,
-    // we assume the intent isto drill into a subtopic, and not a topic
-    //
-    selectingSubtopicTimer = setTimeout(function() {
-      openTopic(topic);
-    }, 400);
-
+    $('#toc > ul > li > ul').hide();
+    $(this).next('ul').show();
+    $('#toc > ul > li > a').removeClass('selected');
+    $(this).addClass('selected');
+    $('#mask').fadeIn('fast');
     return false;
   });
   
-  //
-  // When the user's mouse leaves the Table of Contents completely,
-  // after 400ms, assume the intent is to close the menu
-  //
+  $('#toc > ul > li > a').click(noop);
+
   $('#toc').mouseleave(function() {
-    clearTimeout(selectingTopicTimer);
-    selectingTopicTimer = setTimeout(function() {
-      closeTopic();
+    clearTimeout(t);
+    t = setTimeout(function() {
+      $('#toc > ul > li > ul').hide();
+      $('#mask').fadeOut('fast');
+      $('a').removeClass('selected');
     }, 400);
   });
-
-  //
-  // For users with an impatient intent,
-  // instantly close ToC if they click anywhere outside the ToC
-  //
-  $('body').click(function(e){
-    if(isTopicSelected()){
-      closeTopic();
-    }
-  });
-
-  //
-  // Opens the Table Of Contents
-  //
-  function openTopic(topic) {
-    $('#toc > ul > li > ul').hide();
-    $(topic).next('ul').show();
-    $('#toc > ul > li > a').removeClass('selected');
-    $(topic).addClass('selected');
-    $('#mask').fadeIn('fast');
-  }
-
-  //
-  // Closes the Table Of Contents
-  //
-  function closeTopic() {
-    $('#toc > ul > li > ul').hide();
-    $('#mask').fadeOut('fast');
-    $('a').removeClass('selected');
-  }
-
-  function isTopicSelected() {
-    if($('#toc > ul > li > a.selected').length === 1) {
-      return true;
-    }
-    return false
-  }
 
 });
